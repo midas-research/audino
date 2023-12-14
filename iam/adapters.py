1d1334c052fb3ef8786f6479d219db66fedef741
Rohan Kumar <rk220217@gmail.com>
2023-12-14 15:29:27 +0530

    removed unneccessary files

--- iam/adapters.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.conf import settings
-
-from allauth.account.adapter import DefaultAccountAdapter
-
-class DefaultAccountAdapterEx(DefaultAccountAdapter):
-    def respond_email_verification_sent(self, request, user):
-        return HttpResponseRedirect(settings.ACCOUNT_EMAIL_VERIFICATION_SENT_REDIRECT_URL)
